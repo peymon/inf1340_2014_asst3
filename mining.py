10d73a1fe7520155d45743052c87cff226c03b12
Devin <wanghaoran1128@163.com>
2014-11-25 00:09:41 -0600

    Major part done, add one bonus: Compare Two Stocks, also some tests for it

--- mining.py
+++ mining.py
@@ -22,6 +22,7 @@ __status__ = "Prototype"
 import json
 import datetime
 import itertools
+import math
 
 #stock_data = []
 #monthly_averages = []
@@ -34,47 +35,52 @@ class StockMiner():
 
     def __init__(self, stock_name, stock_file_name):
         """
-
         :param stock_name:
         :param stock_file_name:
         :   return:
         """
         self.stock_name = stock_name
         self.monthly_averages = []
-        self.stock_data = self.read_json_from_file(stock_file_name)
+        self.stock_data = self.read_stock_data(stock_file_name)
 
+        self.change_date()
+        self.get_averages()
+
+    def change_date(self):
+        numerator = 0
+        denominator = 0
         for item in self.stock_data:
             if type(item) is not dict:
                 raise TypeError("Invalid type")
             if "Date" in item.keys():
                 # Change valid date to YYYY/MM format
-                y_m_format = datetime.datetime.strptime(item["Date"], "%Y-%m-%d").strftime("%Y-%m")
+                y_m_format = datetime.datetime.strptime(item["Date"], "%Y-%m-%d").strftime("%Y/%m")
                 item["Date"] = y_m_format
             else:
                 raise ValueError ("Invalid value")
 
+    def get_averages(self):
+        monthly_numerator = []
+        monthly_denominator = []
         for key, value in itertools.groupby(self.stock_data, lambda item: item["Date"]):
-            if type(item["Volume"]) is int and (type(item["Close"])) is int or type(item["Close"]) is float:
-                self.monthly_averages.append((key, sum((item["Volume"] * item["Close"]/item["Volume"]) for item in value)))
-            else:
-                raise TypeError ("Invalid Type")
+            numerator = sum([int(item["Volume"])*float(item["Close"]) for item in value])
+            monthly_numerator.append((key, numerator))
+
+        for key, value in itertools.groupby(self.stock_data, lambda item: item["Date"]):
+            denominator = sum([int(item["Volume"]) for item in value])
+            monthly_denominator.append((key, denominator))
+        i=0
+        while i < len(monthly_numerator):
+            if monthly_numerator[i][0] == monthly_denominator[i][0]:
+                self.monthly_averages.append((monthly_numerator[i][0], round(monthly_numerator[i][1]/monthly_denominator[i][1],2)))
+            i += 1
 
             """
             Calculation of monthly average prices of a stock.
             average price = (V1 * C1 + V2 * C2)/(V1 + C2), where V is volume price and C is close price.
 
-            if "Volume" in item.keys() and "Close" in item.keys:
-                if type(item["Volume"]) is int and (type(item["Close"])) is int or type(item["Close"]) is float):
-                    numerator += item["Volume"] * item["Close"]
-                    denominator += item["Volume"]
-            else:
-                    raise TypeError ("Invalid Type)"
-
-
             Create a tuple with two items: the average for that month and the date (only the month and year).
             Append the tuple for each month to a list.
-
-                self.monthly_averages.append(y_m_format, round(numerator/denominator, 2)))
             """
 
     def six_best_months(self):
@@ -86,7 +92,6 @@ class StockMiner():
         return six_best
         # return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
-
     def six_worst_months(self):
         """
         Using monthly averages, report the six worst months with the lowest average stock price.
@@ -96,8 +101,7 @@ class StockMiner():
         return six_worst
         #return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
-
-    def read_json_from_file(self, file_name):
+    def read_stock_data(self, file_name):
         """
         Read JSON from file.
         :param file_name: JSON file
@@ -106,3 +110,27 @@ class StockMiner():
         with open(file_name) as file_handle:
             file_contents = file_handle.read()
         return json.loads(file_contents)
+
+
+def std(stock):
+        i = 0
+        stock_ave = 0
+        stock_dev = 0
+        while i < len(stock):
+            stock_ave += stock[i][1]
+            i += 1
+        mean1 = stock_ave/len(stock)
+        for item in stock:
+            stock_dev += (item[1] - mean1)**2
+        return math.sqrt(stock_dev/len(stock))
+
+
+def compare(stock1, stock1_file, stock2, stock2_file):
+        stock1_list = StockMiner(stock1, stock1_file)
+        stock2_list = StockMiner(stock2, stock2_file)
+        if std(stock1_list.monthly_averages) < std(stock2_list.monthly_averages):
+            return [stock1]
+        elif std(stock1_list.monthly_averages) > std(stock2_list.monthly_averages):
+            return [stock2]
+        else:
+            return ["Same"]
