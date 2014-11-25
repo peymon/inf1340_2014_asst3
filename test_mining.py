f7c6354259fe99c8c9bc560ff2571ac3a6b62ef8
Peymon <peymon@gmail.com>
2014-11-25 14:25:50 -0500

    added test function

--- test_mining.py
+++ test_mining.py
@@ -30,14 +30,16 @@ def test_goog():
                                   ('2005/03', 181.18)]
 
 # TSE-SO test
-"""
+
 def test_tse_so():
-    tseso = StockMiner("TSE-SO", "data/TSE-SO.json"
-    assert tseso.six_best_months() ==
-    assert tseso.six_worst_months() ==
-"""
+    tseso = StockMiner("TSE-SO", "data/TSE-SO.json")
+    assert tseso.six_best_months() == [('2007/12', 20.98), ('2007/11', 20.89), ('2013/05', 19.96), ('2013/06', 19.94),
+                                       ('2013/04', 19.65), ('2007/10', 19.11)]
+    assert tseso.six_worst_months() == [('2009/03', 1.74), ('2008/11', 2.08), ('2008/12', 2.25), ('2009/02', 2.41),
+                                        ('2009/04', 2.75), ('2009/01', 3.14)]
+
 
 # Bonus: Compare Two Stocks test
 def test_com():
-    assert compare("GOOG", "data/GOOG.json", "TSE-SO", "data/TSE-SO.json") == ["TSE-SO"]
+    assert compare("GOOG", "data/GOOG.json", "TSE-SO", "data/TSE-SO.json") == ["GOOG"]
     assert compare("GOOG", "data/GOOG.json", "GOOG", "data/GOOG.json") == ["Same"]
